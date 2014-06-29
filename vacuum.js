514299104fc8d78a8eff0c2d5733b9b39a13ed9f
Forrest L Norvell <forrest@npmjs.com>
2014-06-28 20:43:37 -0700

    validate leaf is in base, support files

--- vacuum.js
+++ vacuum.js
@@ -10,73 +10,93 @@ var unlink  = require("graceful-fs").unlink
 
 module.exports = vacuum
 
-function vacuum(leafDir, options, cb) {
-  assert(typeof leafDir === "string", "must pass in directory to remove")
+function vacuum(leaf, options, cb) {
+  assert(typeof leaf === "string", "must pass in path to remove")
   assert(typeof cb === "function", "must pass in callback")
 
   if (!options) options = {}
   assert(typeof options === "object", "options must be an object")
 
-  var base = options.base
   var log = options.log ? options.log : function () {}
 
-  lstat(leafDir, function (error, stat) {
+  var base = options.base
+  if (base && resolve(leaf).indexOf(resolve(base)) !== 0) {
+    return cb(new Error(resolve(leaf) + " is not a child of " + resolve(base)))
+  }
+
+  lstat(leaf, function (error, stat) {
     if (error) {
+      if (error.code === "ENOENT") return cb(null)
+
       log(error.stack)
       return cb(error)
     }
 
-    if (!(stat && (stat.isDirectory() || stat.isSymbolicLink()))) {
-      log(leafDir, "is not a directory or link")
-      return cb(new Error(leafDir + " is not a directory or link"))
+    if (!(stat && (stat.isDirectory() || stat.isSymbolicLink() || stat.isFile()))) {
+      log(leaf, "is not a directory, file, or link")
+      return cb(new Error(leaf + " is not a directory, file, or link"))
     }
 
     if (options.purge) {
-      log("purging", leafDir)
-      rimraf(leafDir, function (error) {
+      log("purging", leaf)
+      rimraf(leaf, function (error) {
         if (error) return cb(error)
 
-        next(dirname(leafDir))
+        next(dirname(leaf))
+      })
+    }
+    else if (!stat.isDirectory()) {
+      log("removing", leaf)
+      unlink(leaf, function (error) {
+        if (error) return cb(error)
+
+        next(dirname(leaf))
       })
     }
     else {
-      next(leafDir)
+      next(leaf)
     }
   })
 
-  function next(entry) {
+  function next(branch) {
     // either we've reached the base or we've reached the root
-    if (resolve(entry) === resolve(base) || entry === dirname(entry)) {
-      log("finished vacuuming up to", entry)
+    if ((base && resolve(branch) === resolve(base)) || branch === dirname(branch)) {
+      log("finished vacuuming up to", branch)
       return cb(null)
     }
 
-    readdir(entry, function (error, files) {
+    readdir(branch, function (error, files) {
       if (error) {
-        log("unable to check directory", entry, "due to", error.message)
+        if (error.code === "ENOENT") return cb(null)
+
+        log("unable to check directory", branch, "due to", error.message)
         return cb(error)
       }
 
       if (files.length > 0) {
-        log("quitting because other entries in", entry)
+        log("quitting because other entries in", branch)
         return cb(null)
       }
 
-      log("removing", entry)
-      lstat(entry, function (error, stat) {
+      log("removing", branch)
+      lstat(branch, function (error, stat) {
         if (error) {
-          log("unable to lstat directory", entry, "due to", error.message)
+          if (error.code === "ENOENT") return cb(null)
+
+          log("unable to lstat", branch, "due to", error.message)
           return cb(error)
         }
 
-        var remove = stat.isSymbolicLink() ? unlink : rmdir
-        remove(entry, function (error) {
+        var remove = stat.isDirectory() ? rmdir : unlink
+        remove(branch, function (error) {
           if (error) {
-            log("unable to remove directory", entry, "due to", error.message)
+            if (error.code === "ENOENT") return cb(null)
+
+            log("unable to remove", branch, "due to", error.message)
             return cb(error)
           }
 
-          next(dirname(entry))
+          next(dirname(branch))
         })
       })
     })
